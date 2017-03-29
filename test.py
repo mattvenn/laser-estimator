dac66eb67cd72c4c4fa78b69bb57b1d5ccfca54e
matt venn <matt@mattvenn.net>
2017-03-29 17:02:02 +0200

    test material cost

--- test.py
+++ test.py
@@ -33,8 +33,8 @@ class TestCase(TestCase):
     def test_bounding_box(self):
         config = [
             { 'file': '50x50_empty.svg', 'bb': '50.0 x 50.0 mm' },
-#            { 'file': '50x50_empty_RGB.svg', 'bb': '161.22 x 51.28 mm' },
-#            { 'file': 'bounding.svg', 'bb': '185.16 x 139.12 mm' },
+            { 'file': '50x50_empty_RGB.svg', 'bb': '161.22 x 51.28 mm' },
+            { 'file': 'bounding.svg', 'bb': '185.16 x 139.12 mm' },
             ]
         for c in config:
             payload = { 'svg': open('testfiles/%s' % c['file']), 'material_id': 1}
@@ -43,28 +43,29 @@ class TestCase(TestCase):
 
     def test_colour_paths(self):
         config = [
-            { 'file': '50x50_empty.svg', 'bb': 'colour #000000 = 200.0 mm' },
-            { 'file': '50x50_empty_RGB.svg', 'bb': ' #0000ff = 200.0 mm' },
-            { 'file': '50x50_empty_RGB.svg', 'bb': ' #000000 = 200.0 mm' },
-            { 'file': '50x50_empty_RGB.svg', 'bb': ' #ff0000 = 200.0 mm' },
+            { 'file': '50x50_empty.svg', 'col': 'colour #000000 = 200.0 mm' },
+            { 'file': '50x50_empty_RGB.svg', 'col': ' #0000ff = 200.0 mm' },
+            { 'file': '50x50_empty_RGB.svg', 'col': ' #000000 = 200.0 mm' },
+            { 'file': '50x50_empty_RGB.svg', 'col': ' #ff0000 = 200.0 mm' },
             ]
         for c in config:
             payload = { 'svg': open('testfiles/%s' % c['file']), 'material_id': 1 }
             rv = self.client.post("/", data=payload)
-            assert c['bb'] in rv.data
+            assert c['col'] in rv.data
 
     def test_svgs(self):
         config = [
-            { 'file': '50x50_empty.svg', 'len': '200.0 mm' },
-            { 'file': '50x50circle.svg', 'len': '156.98 mm' },
-            { 'file': 'text_as_path.svg', 'len': '513.31 mm' },
-            { 'file': '50x50_empty_RGB.svg', 'len': '600.0 mm' },
+            { 'file': '50x50_empty.svg', 'len': '200.0 mm', 'cost': '10.0' },
+            { 'file': '50x50circle.svg', 'len': '156.98 mm', 'cost': '9.32' }, # wrong! should be 50x50 and same price as square
+            { 'file': 'text_as_path.svg', 'len': '513.31 mm', 'cost': '11.42' },
+            { 'file': '50x50_empty_RGB.svg', 'len': '600.0 mm', 'cost': '33.07' },
             ]
         
         for c in config:
             payload = { 'svg': open('testfiles/%s' % c['file']), 'material_id': 1 }
             rv = self.client.post("/", data=payload)
             assert c['len'] in rv.data
+            assert c['cost'] in rv.data
 
     def test_material_list(self):
         rv = self.client.get("/", follow_redirects=True)
